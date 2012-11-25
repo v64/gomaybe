9f2e63c41f045b905bbfe0e90aa62a8b0c6f3511
Jahn Veach <v64@v64.net>
2012-11-25 13:51:23 -0600

    opCodeCycles table

--- cpu.go
+++ cpu.go
@@ -11,6 +11,10 @@ const (
     FLAG_Z = uint8(7)
 )
 
+var (
+    opCodeCycles map[byte]int
+)
+
 type Cpu struct {
     aReg, bReg, cReg, dReg, eReg, fReg, hReg, lReg uint8
     spReg, pcReg uint16
@@ -21,6 +25,35 @@ func (cpu *Cpu) Init(ram *Ram) {
     cpu.spReg = 0xFFFE
     cpu.pcReg = 0x0100
     cpu.ram = ram
+
+    opCodeCycles = map[byte]int {
+        0xA7: 4,
+        0xA0: 4,
+        0xA1: 4,
+        0xA2: 4,
+        0xA3: 4,
+        0xA4: 4,
+        0xA5: 4,
+        0xB7: 4,
+        0xB0: 4,
+        0xB1: 4,
+        0xB2: 4,
+        0xB3: 4,
+        0xB4: 4,
+        0xB5: 4,
+        0xAF: 4,
+        0xA8: 4,
+        0xA9: 4,
+        0xAA: 4,
+        0xAB: 4,
+        0xAC: 4,
+        0xAD: 4,
+        0x2F: 4,
+        0x3F: 4,
+        0x37: 4,
+        0x00: 4,
+        0xC3: 16,
+    }
 }
 
 func (cpu *Cpu) GetFlag(flag uint8) (result bool) {
@@ -91,97 +124,75 @@ func (cpu *Cpu) Step() (cycleCount int) {
     switch opCode {
         // START 3.3.3.5 AND n
         case 0xA7:
-            cycleCount = 4
             cpu.AND_A(cpu.aReg)
 
         case 0xA0:
-            cycleCount = 4
             cpu.AND_A(cpu.bReg)
 
         case 0xA1:
-            cycleCount = 4
             cpu.AND_A(cpu.cReg)
 
         case 0xA2:
-            cycleCount = 4
             cpu.AND_A(cpu.dReg)
 
         case 0xA3:
-            cycleCount = 4
             cpu.AND_A(cpu.eReg)
 
         case 0xA4:
-            cycleCount = 4
             cpu.AND_A(cpu.hReg)
 
         case 0xA5:
-            cycleCount = 4
             cpu.AND_A(cpu.lReg)
         // END 3.3.3.5 AND n
 
         // START 3.3.3.6 OR n
         case 0xB7:
-            cycleCount = 4
             cpu.OR_A(cpu.aReg)
 
         case 0xB0:
-            cycleCount = 4
             cpu.OR_A(cpu.bReg)
 
         case 0xB1:
-            cycleCount = 4
             cpu.OR_A(cpu.cReg)
 
         case 0xB2:
-            cycleCount = 4
             cpu.OR_A(cpu.dReg)
 
         case 0xB3:
-            cycleCount = 4
             cpu.OR_A(cpu.eReg)
 
         case 0xB4:
-            cycleCount = 4
             cpu.OR_A(cpu.hReg)
 
         case 0xB5:
-            cycleCount = 4
             cpu.OR_A(cpu.lReg)
         // END 3.3.3.6 OR n
 
         // START 3.3.3.7 XOR n
         case 0xAF:
-            cycleCount = 4
             cpu.XOR_A(cpu.aReg)
 
         case 0xA8:
-            cycleCount = 4
             cpu.XOR_A(cpu.bReg)
 
         case 0xA9:
-            cycleCount = 4
             cpu.XOR_A(cpu.cReg)
 
         case 0xAA:
-            cycleCount = 4
             cpu.XOR_A(cpu.dReg)
 
         case 0xAB:
-            cycleCount = 4
             cpu.XOR_A(cpu.eReg)
 
         case 0xAC:
-            cycleCount = 4
             cpu.XOR_A(cpu.hReg)
 
         case 0xAD:
-            cycleCount = 4
             cpu.XOR_A(cpu.lReg)
         // END 3.3.3.7 XOR n
 
         // START 3.3.5.3 CPL
         case 0x2F:
-            cycleCount = 4
             cpu.aReg = ^cpu.aReg
             cpu.SetFlag(FLAG_N, true)
             cpu.SetFlag(FLAG_H, true)
@@ -189,7 +200,6 @@ func (cpu *Cpu) Step() (cycleCount int) {
 
         // START 3.3.5.4 CCF
         case 0x3F:
-            cycleCount = 4
             cpu.SetFlag(FLAG_N, false)
             cpu.SetFlag(FLAG_H, false)
             cpu.SetFlag(FLAG_C, !cpu.GetFlag(FLAG_C))
@@ -197,7 +207,6 @@ func (cpu *Cpu) Step() (cycleCount int) {
 
         // START 3.3.5.5 SCF
         case 0x37:
-            cycleCount = 4
             cpu.SetFlag(FLAG_N, false)
             cpu.SetFlag(FLAG_H, false)
             cpu.SetFlag(FLAG_C, true)
@@ -205,12 +214,10 @@ func (cpu *Cpu) Step() (cycleCount int) {
 
         // START 3.3.5.6 NOP
         case 0x00:
-            cycleCount = 4
         // END 3.3.5.6 NOP
 
         // START 3.3.8.1 JP nn
         case 0xC3:
-            cycleCount = 16
             least := cpu.ram.Read(cpu.pcReg)
             cpu.pcReg++
             most := cpu.ram.Read(cpu.pcReg)
@@ -218,9 +225,10 @@ func (cpu *Cpu) Step() (cycleCount int) {
         // END 3.3.8.1 JP nn
 
         default:
-            cycleCount = 0
             fmt.Printf("Unknown OP: 0x%.2X\n", opCode)
     }
+
+    cycleCount = opCodeCycles[opCode]
 
     return
 }
